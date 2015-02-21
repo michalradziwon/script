7bf4255cecce512d679caa26df248f174a711623
Michal <michal.radzi.won@gmail.com>
2015-02-21 21:18:36 +0000

    added lookForFieldDefinition function

--- ConvertFieldsToGetters.es6.js
+++ ConvertFieldsToGetters.es6.js
@@ -21,12 +21,29 @@ var lookForAssignment = function (line) {
   }
 }
 
+var lookForFieldDefinition = function (line) {
+  try {
+    var parsedURL = /\s*public\s+([\w\<\>]+)\s*(\w+)[\s;].*/.exec(line);
+    var [, ttype, name] = parsedURL;
+    return {type: ttype, name:name};
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 
 var tests = function () {
   var assert = require('assert');
   function testEq(act, exp) {
     assert(JSON.stringify(exp) === JSON.stringify(act), "\nExp   : " + JSON.stringify(exp) + "\n" + "Actual: "+ JSON.stringify(act))
   };
+  testEq(lookForFieldDefinition("public Date                              myDateField;"),
+    {type: "Date", name:"myDateField"});
+  testEq(lookForFieldDefinition("public List<TypeOne>      myList         = new ArrayList<TypeOne>();"),
+    {type: "List<TypeOne>", name:"myList"});
+  testEq(lookForFieldDefinition("    public double                            doubleField;"),
+    {type: "double", name:"doubleField"});
+
   testEq(lookForAssignment("    myVar.fieldOne = newValueProvider.getX();"),
     {variable: "myVar", field: "fieldOne", value: "newValueProvider.getX()"});
 
