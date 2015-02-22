244b6a33b4a18c77e3fb60716c453a782fcfa0f9
Michal <michal.radzi.won@gmail.com>
2015-02-22 14:55:22 +0000

    finished porting this little script from scala to es6

--- ConvertFieldsToGetters.es6.js
+++ ConvertFieldsToGetters.es6.js
@@ -11,6 +11,14 @@
 
  */
 
+var convertFieldAssignmentToGetterMocking = function (line) {
+  var res = lookForAssignment(line);
+  if (res) {
+    return `when(${res.variable}.${mapFieldNameToGetter(res.field, null)}()).thenReturn(${res.value});`;
+  } else {
+    return line;
+  }
+}
 
 var lookForAssignment = function (line) {
   try {
@@ -21,31 +29,54 @@ var lookForAssignment = function (line) {
   }
 }
 
+var convertToGetterIfPossible = function (line) {
+  var res = lookForFieldDefinition(line);
+  if (res) {
+    return `public ${res.type} ${mapFieldNameToGetter(res.name, res.type)}(){
+  return TODO;
+}`;
+  } else {
+    return line;
+  }
+}
+
+var mapFieldNameToGetter = function (field, typee) {
+  if (typee && typee.toLowerCase == "boolean") return "is" + field[0].toUpperCase() + field.substring(1);
+  else return "get" + field[0].toUpperCase() + field.substring(1);
+};
+
+
 var lookForFieldDefinition = function (line) {
   try {
     var parsedURL = /\s*public\s+([\w\<\>]+)\s*(\w+)[\s;].*/.exec(line);
     var [, ttype, name] = parsedURL;
-    return {type: ttype, name:name};
+    return {type: ttype, name: name};
   } catch (e) {
-    console.log(e);
   }
 }
 
-
-var tests = function () {
+var runTests = function () {
   var assert = require('assert');
+
   function testEq(act, exp) {
-    assert(JSON.stringify(exp) === JSON.stringify(act), "\nExp   : " + JSON.stringify(exp) + "\n" + "Actual: "+ JSON.stringify(act))
+    assert(JSON.stringify(exp) === JSON.stringify(act), "\nExp   : " + JSON.stringify(exp) + "\n" + "Actual: " + JSON.stringify(act))
   };
   testEq(lookForFieldDefinition("public Date                              myDateField;"),
-    {type: "Date", name:"myDateField"});
+    {type: "Date", name: "myDateField"});
   testEq(lookForFieldDefinition("public List<TypeOne>      myList         = new ArrayList<TypeOne>();"),
-    {type: "List<TypeOne>", name:"myList"});
+    {type: "List<TypeOne>", name: "myList"});
   testEq(lookForFieldDefinition("    public double                            doubleField;"),
-    {type: "double", name:"doubleField"});
-
+    {type: "double", name: "doubleField"});
+  testEq(convertToGetterIfPossible(`@Documentation("Doc 123.")`),
+    `@Documentation("Doc 123.")`);
+  testEq(convertToGetterIfPossible(`    public double                            doubleField;`),
+    `public double getDoubleField(){
+  return TODO;
+}`);
   testEq(lookForAssignment("    myVar.fieldOne = newValueProvider.getX();"),
     {variable: "myVar", field: "fieldOne", value: "newValueProvider.getX()"});
+  testEq(convertFieldAssignmentToGetterMocking("    myVar.fieldOne = newValueProvider.getX();"),
+    "when(myVar.getFieldOne()).thenReturn(newValueProvider.getX());");
 
 };
-tests();
+runTests();
